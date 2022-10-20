f5eb6b63d1706c3b280370c0e7285cfa3641d07b
tamaranold <38466492+tamaranold@users.noreply.github.com>
2022-10-20 18:57:04 +0200

    add comments

--- syntax/prep_rotate.R
+++ syntax/prep_rotate.R
@@ -1,13 +1,13 @@
 # Image Recognition
-# rotate images
+# preprocessing rotate images
 
 # read packages
 library(magick)
 library(tidyverse)
 
 # define path 
-indir <- "C:/Users/nold_/Desktop/Bilder/ac/original/"
-outdir <- "C:/Users/nold_/Desktop/Bilder/ac/train/"
+indir <- ".../original/"
+outdir <- ".../train/"
 
 # get folders 
 infolder <- list.dirs(indir,
@@ -19,20 +19,23 @@ rotation <- c(0, 90, 180, 270)
 # recreate each folder and rotate images
 for(i in infolder){
   
+  # create folder
   dir.create(paste0(outdir, i))
   file.rename(list.files(paste0(indir, i), full.names = TRUE),
               paste0(indir, i, "/", i, "_ac_train_", 1:length(list.files(paste0(indir, i))), ".jpg"))
   
+  # prep each image
   files <- list.files(paste0(indir, i))
   
   for(f in files){
     image <- image_read(paste0(indir, i, "/", f))
     
     for(r in rotation){
+      # rotate
       img_rotate <- image_rotate(image, r)
-      #rename
+      # rename
       nam <- str_replace(f, ".jpg", paste0("_", r, ".jpg"))
-      #save
+      # save
       image_write(img_rotate, 
                   paste0(outdir, i, "/", nam))
     }
